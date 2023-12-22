a8db45d00ceb1bfdad82cfdbc63598ef0db879ff
Volodymyr Nerovnia <nerv@i.ua>
2023-12-22 16:25:53 +0200

    Created UrlInput component

---
+++ src/components/form/Inputs/UrlInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <Input inpType="url" :inpId="inpId" :inpPlaceholder="inpPlaceholder"/>
+</template>
+
+<script>
+import Input from "./Input.vue";
+
+export default {
+  components: {
+    Input
+  },
+  props: {
+    inpId: {
+      type: String,
+      required: true,
+    },
+    inpPlaceholder: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
