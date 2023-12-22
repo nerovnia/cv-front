9bfd2e11725d971b9f6312f6110726f555f00a18
Volodymyr Nerovnia <nerv@i.ua>
2023-12-22 16:25:29 +0200

    Created Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <input :class="btnProps.class" :type="btnProps.type" :value="btnProps.value">
+</template>
+
+<script>
+export default {
+  props: {
+    btnProps: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+input.submit, input.button {
+  color: rgb(4, 116, 0);
+  background-color: #bcfdc7;
+  border: 1px solid rgb(4, 116, 0);
+  padding: 5px;
+}
+</style>
