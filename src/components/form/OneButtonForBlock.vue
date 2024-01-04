ef2f6e530715085df534c52a0f4644e7d8111528
Volodymyr Nerovnia <nerv@i.ua>
2024-01-04 20:43:57 +0200

    Create OneButtonForBlock form component

---
+++ src/components/form/OneButtonForBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <component:is="blockComponent" />
+  </div>
+  <div>
+    <Button :btnProps="buttons.add" @click="addResource" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
