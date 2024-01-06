14a61be9b82b131c47ae82c35ee00a18bd62517b
Volodymyr Nerovnia <nerv@i.ua>
2024-01-07 00:01:15 +0200

    Created new component EducationBlock

---
+++ src/views/forms/dict/education/EducationBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TextInput :inpId="inpId" :inpPlaceholder="inpPlaceholder"/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
